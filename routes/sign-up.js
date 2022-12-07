e8cdddc5c09a5e0c6e83027d3d91fe3043da0404
pequito193 <vitorspequito@gmail.com>
2022-12-07 13:48:04 +0000

    Add form sanitation and error handling on create user

--- routes/sign-up.js
+++ routes/sign-up.js
@@ -25,27 +25,40 @@ router.post('/sign-up', function(req, res, next) {
   const password = req.body.password;
   const confirmPassword = req.body.confirmPassword;
   console.log(req.body);
-  if (password === confirmPassword) {
-    const pass = bcrypt.hash(`${password}`, 10, (err, hashedPassword) => {
-      if(err) {
-        return next(err);
-      }
-      else {
-      const user = new User({
-        username: req.body.username,
-        email: req.body.email,
-        password: hashedPassword
-      }).save(err => {
-        if (err) { 
+
+  // Check if password fields match
+  if (!(password === confirmPassword)) {
+    res.render('sign-up', {message: 'Passwords do not match!'});
+    return;
+  };
+
+  // Check if username already exists
+  User.countDocuments({username: req.body.username}, function(err, count) {
+    if (count > 0) {
+      res.render('sign-up', {message: 'Username already exists!'});
+      return;
+    }
+
+    else {
+      // Encrypt password and create the user
+      bcrypt.hash(`${password}`, 10, (err, hashedPassword) => {
+        if(err) {
           return next(err);
         }
-        res.redirect("/");
-      })};
-    });
-  }
-  else {
-    res.render('sign-up', {message: 'Passwords do not match!'});
-  }
+        else {
+        const user = new User({
+          username: req.body.username,
+          email: req.body.email,
+          password: hashedPassword
+        }).save(err => {
+          if (err) { 
+            return next(err);
+          }
+          res.redirect("/sign-in");
+        })};
+      });
+    }
+  });
 })
 
 router.post('/sign-in', function(req, res, next) {
