6faf32da38a51cf94b43d32f27f7604d9705692f
pequito193 <vitorspequito@gmail.com>
2022-12-07 11:29:49 +0000

    Update sign-up view and router

--- routes/sign-up.js
+++ routes/sign-up.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var router = express.Router();
+const bcrypt = require('bcryptjs');
+const User = require('../models/user_model');
 
+
+
+// GET requests to render pages
 router.get('/', function(req, res, next) {
   res.redirect('/sign-up');
 })
@@ -11,6 +16,40 @@ router.get('/sign-up', function(req, res, next) {
 
 router.get('/sign-in', function(req, res, next) {
   res.render('sign-in');
+})
+
+
+
+// POST requests to submit a form
+router.post('/sign-up', function(req, res, next) {
+  const password = req.body.password;
+  const confirmPassword = req.body.confirmPassword;
+  console.log(req.body);
+  if (password === confirmPassword) {
+    const pass = bcrypt.hash(`${password}`, 10, (err, hashedPassword) => {
+      if(err) {
+        return next(err);
+      }
+      else {
+      const user = new User({
+        username: req.body.username,
+        email: req.body.email,
+        password: hashedPassword
+      }).save(err => {
+        if (err) { 
+          return next(err);
+        }
+        res.redirect("/");
+      })};
+    });
+  }
+  else {
+    res.render('sign-up', {message: 'Passwords do not match!'});
+  }
+})
+
+router.post('/sign-in', function(req, res, next) {
+
 })
 
 module.exports = router;
