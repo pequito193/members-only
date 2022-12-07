555b0aa7246d6dcf351d1c5c93e2d1d2053518e7
pequito193 <vitorspequito@gmail.com>
2022-12-07 11:29:00 +0000

    Add .env to secure passwords

--- app.js
+++ app.js
@@ -9,6 +9,15 @@ var forumRouter = require('./routes/forum');
 
 var app = express();
 
+require('dotenv').config();
+
+// Set up mongoose connection
+const mongoose = require("mongoose");
+const mongoDB = process.env.DB_LINK;
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
