c728b805fad8dcea34386cb549f35882f8d1df46
pequito193 <vitorspequito@gmail.com>
2022-12-08 11:31:45 +0000

    Add passport middleware and secure session secret key

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const bcrypt = require('bcryptjs');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const session = require('express-session');
 
 var signUpRouter = require('./routes/sign-up');
 var forumRouter = require('./routes/forum');
@@ -17,6 +21,48 @@ const mongoDB = process.env.DB_LINK;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
+passport.use(
+  new LocalStrategy((username, password, done) => {
+    User.findOne({ username: username }, (err, user) => {
+      if (err) { 
+        return done(err);
+      }
+      if (!user) {
+        console.log('user not found');
+        return done(null, false, { message: "Incorrect username" });
+      }
+      console.log('user found');
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (res) {
+          console.log('password matches');
+          // passwords match! log user in
+          return done(null, user)
+        } else {
+          console.log('password does not match');
+          // passwords do not match!
+          return done(null, false, { message: "Incorrect password" })
+        }
+      })
+    });
+  })
+);
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+  
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+
+app.use(session({ secret: process.env.SECRET_KEY, resave: false, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.urlencoded({ extended: false }));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
